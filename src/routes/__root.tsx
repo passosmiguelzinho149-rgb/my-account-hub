20948fdc48674c26a6e40dade2e43b420db3ff20
passosmiguelzinho149-rgb <passosmiguelzinho149@gmail.com>
2026-09-26 03:13:44 -0400

    Registra e disponibiliza instalação do PWA

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -12,6 +12,7 @@ import { useEffect, type ReactNode } from "react";
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { SessionProvider } from "../lib/session";
+import { PwaInstallPrompt } from "../components/app/PwaInstallPrompt";
 
 function NotFoundComponent() {
   return (
@@ -136,6 +137,7 @@ function RootComponent() {
       <SessionProvider>
         {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
         <Outlet />
+        <PwaInstallPrompt />
       </SessionProvider>
     </QueryClientProvider>
   );
